7023dd38c5cb4ad0d1f90b09b6d09187325daa0d
annamjdy <57040076+annamjdy@users.noreply.github.com>
2019-11-28 14:17:04 +0100

    Feature get spells (#4)
    * Create getSpells.js
    
    The getSpells() function that returns the list of spells
    
    * Update index.js
    
    importing getSpells function

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
-import '../css/style.css';
+import '../css/style.css'
+import '../images/harry-potter-3670411_1920.png';
+import './getSpells';
 import '../images/harry-potter-3670411_1920.png'
 import './getHouse'
 import './generateCharacterEndpoint'
 import './getCharacters'
-
