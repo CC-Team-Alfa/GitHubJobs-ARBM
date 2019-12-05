95d8d90c3592281886dada2762baea3c60fdde12
Bartosz <swietlik985@gmail.com>
2019-12-06 00:52:47 +0100

    generateCharacterList function added. generateShortCharacterView modified, now it returns <div>.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import '../css/style.css'
 import '../images/harry-potter-3670411_1920.png';
 import './getSpells';
-import '../images/harry-potter-3670411_1920.png'
 import './getHouse'
 import './generateCharacterEndpoint'
 import './getCharacters'
+import './generateShortCharacterView';
